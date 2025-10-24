59e061f0998e6423a10e7e94cfbb4eb0ebbfc6f2
Kevin Kuc <kkuc234@gmail.com>
2025-10-24 12:12:21 -0400

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kevs_personal_website/', // Replace with your actual repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })

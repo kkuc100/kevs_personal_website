45608035a8ac130e7a943b61f961edf12218ef0c
Kevin Kuc <kkuc234@gmail.com>
2025-10-25 18:56:25 -0400

    new

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -1,7 +1,7 @@
 // Utility function to get the correct image path based on environment
 export const getImagePath = (imagePath: string): string => {
   // In development, BASE_URL is '/'
-  // In production, BASE_URL is '/kevs_personal_website/'
+  // In production, BASE_URL is '/'
   const baseUrl = import.meta.env.BASE_URL
   return `${baseUrl}${imagePath.substring(1)}`
 }
